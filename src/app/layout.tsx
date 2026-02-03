15e75a5ce8ab470c7cb436c324a6fa00d19842f1
williamtriinh <developer@williamtrinh.dev>
2026-02-03 09:12:28 -0500

    Update dependencies and fix build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
         >
           <Navbar />
